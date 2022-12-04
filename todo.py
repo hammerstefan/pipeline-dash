ab71473a364e838e20625c4d7bb3f29bbf396d8f
Stefan Hammer <stefan.hammer@canonical.com>
2022-12-04 13:33:55 -0600

    Update todo

--- todo.py
+++ todo.py
@@ -1,13 +1,11 @@
 # todo: Use `recurse` option in YAML to determine if to recurse jobs or not.
 #   * Currently will recurse everything if `recurse` command is used
-# todo: Use 'label' option in YAML to set node label on graph
 # todo: Add more data to jobs table
 # todo: Make navigation of jobs table faster
 # todo: * Expand sub-tree button
 # todo: Revisit: Handle graph scaling to viewport size
 # todo: Pipepline depth limit selection
 # todo: fix top level root view
-# todo: support dark mode
 # todo: move settings to modal
 # todo: take another look at improving the custom-callback syntax for components. @decorator somehow?
 # todo: Take another look at top level callbacks and dependency on id string
